e7738210fa1553069b587c456d5ad979ad7753f8
KaliMinion <33697905+KaliMinion@users.noreply.github.com>
2017-12-10 11:26:29 -0600

    Update and rename README.md to Moogle Scripts.lua

---
+++ Moogle Scripts.lua
@@ -0,0 +1,47 @@
+local tbl = 
+{
+	[1] = {
+		name = "Moogle Updater",
+		table = table.valid(MoogleUpdater),
+		url = "",
+		version = "1.0.0",
+		releasedate = os.time { year = 2017, month = 12, day = 9, hour = 10, min = 44 },
+		lastupdate = os.time { year = 2017, month = 12, day = 9, hour = 10, min = 44 },
+	},
+	[2] = {
+		name = "Main Window",
+		table = table.valid(KaliMainWindow),
+		url = "",
+		version = "1.0.0",
+		releasedate = os.time { year = 2017, month = 12, day = 9, hour = 10, min = 44 },
+		lastupdate = os.time { year = 2017, month = 12, day = 9, hour = 10, min = 44 },
+	},
+	[3] = {
+		name = "Moogle Functions",
+		table = table.valid(MoogleFunctions),
+		url = "",
+		version = "1.0.0",
+		releasedate = os.time { year = 2017, month = 12, day = 9, hour = 10, min = 44 },
+		lastupdate = os.time { year = 2017, month = 12, day = 9, hour = 10, min = 44 },
+	},
+	[4] = {
+		name = "Moogle Text to Speech",
+		table = table.valid(MoogleTTS),
+		url = "",
+		version = "1.0.0",
+		releasedate = os.time { year = 2017, month = 12, day = 9, hour = 10, min = 44 },
+		lastupdate = os.time { year = 2017, month = 12, day = 9, hour = 10, min = 44 },
+	},
+	[5] = {
+		name = "Moogle Hoarder",
+		table = table.valid(MoogleHoarder),
+		url = "",
+		version = "1.0.0",
+		releasedate = os.time { year = 2017, month = 12, day = 9, hour = 10, min = 44 },
+		lastupdate = os.time { year = 2017, month = 12, day = 9, hour = 10, min = 44 },
+	},
+}
+
+
+
+return tbl
