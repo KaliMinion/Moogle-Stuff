6cde139cc35602b941aa80199011eaef683f4be7
KaliMinion <33697905+KaliMinion@users.noreply.github.com>
2018-01-05 14:28:48 -0600

    Update Moogle Scripts.lua

--- Moogle Scripts.lua
+++ Moogle Scripts.lua
@@ -6,11 +6,11 @@ local tbl =
 		filepath = [[Moogle Updater.lua]],
 		table = "return MoogleUpdater",
 		url = "https://github.com/KaliMinion/Moogle-Stuff/raw/master/Moogle Updater.lua",
-		version = "1.1.5",
+		version = "1.1.6",
 		category = "Core",
 		stability = "Core",
 		releasedate = os.time { year = 2017, month = 12, day = 9 },
-		lastupdate = 1515144134,
+		lastupdate = 1515184106,
 	},
 	[2] = {
 		name = "Main Window",
