115875c4e5443653eb5c5dc517fd5b3d7d8d8ba6
KaliMinion <33697905+KaliMinion@users.noreply.github.com>
2018-01-07 13:35:25 -0600

    Update MoogleScripts.lua

--- MoogleScripts.lua
+++ MoogleScripts.lua
@@ -10,7 +10,7 @@ local tbl =
 		category = "Core Moogle Module",
 		stability = "Core Moogle Module",
 		releasedate = os.time { year = 2017, month = 12, day = 9 },
-		lastupdate = 1515351955,
+		lastupdate = 1515353712,
 		info = [[Downloads scripts, keeps them updated. :P]],
 	},
 	[2] = {
