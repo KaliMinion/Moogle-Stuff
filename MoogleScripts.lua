05b426b338c34f22fb2eab7042cd71c3ee60fc1b
Kali <33697905+KaliMinion@users.noreply.github.com>
2018-02-19 12:22:03 -0600

    Update MoogleScripts.lua

--- MoogleScripts.lua
+++ MoogleScripts.lua
@@ -7,11 +7,11 @@ local tbl =
 		filepath = [[Moogle Updater.lua]],
 		table = "return MoogleUpdater",
 		url = "https://github.com/KaliMinion/Moogle-Stuff/raw/master/MoogleUpdater.lua",
-		version = "1.2.9",
+		version = "1.2.10",
 		category = "Core Moogle Module",
 		stability = "Core Moogle Module",
 		releasedate = os.time { year = 2017, month = 12, day = 9 },
-		lastupdate = 1516940689,
+		lastupdate = 1519064250,
 		info = [[Downloads scripts, keeps them updated. :P]],
 	},
 	[2] = {
@@ -20,12 +20,12 @@ local tbl =
 		filepath = [[Main Window.lua]],
 		table  = "return KaliMainWindow",
 		url = [[https://github.com/KaliMinion/Moogle-Stuff/raw/master/MainWindow.lua]],
-		version = "1.2.9",
+		version = "1.2.10",
 		category = "Core Moogle Module",
 		stability = "Core Moogle Module",
 		instver = KaliMainWindow.Info.Version or false,
 		releasedate = os.time { year = 2017, month = 12, day = 9 },
-		lastupdate = 1516909839,
+		lastupdate = 1519064004,
 		info = [[The Main Window that all Moogle Scripts share to display the settings for each module.]],
 	},
 	[3] = {
@@ -34,11 +34,11 @@ local tbl =
 		filepath = [[MoogleLib.lua]],
 		table = "return MoogleLib",
 		url = [[https://github.com/KaliMinion/Moogle-Stuff/raw/master/MoogleLib.lua]],
-		version = "1.3.1",
+		version = "1.3.2",
 		category = "Core Moogle Module",
 		stability = "Core Moogle Module",
 		releasedate = os.time { year = 2017, month = 12, day = 9 },
-		lastupdate = 1516909799,
+		lastupdate = 1519064099,
 		info = [[Where all my Moogle Functions are stored.]],
 	},
 	[4] = {
@@ -47,11 +47,11 @@ local tbl =
 		filepath = [[Moogle Scripts\Moogle TTS\Moogle TTS.lua]],
 		table = "return MoogleTTS",
 		url = [[https://github.com/KaliMinion/Moogle-Stuff/raw/master/MoogleTTS.lua]],
-		version = "1.1.4",
+		version = "1.1.5",
 		category = "Utility",
 		stability = "WiP but Working",
 		releasedate = os.time { year = 2017, month = 12, day = 9 },
-		lastupdate = 1516509654,
+		lastupdate = 1519064202,
 		info = [[Narrates NPC dialog, as well as some other windows in game.]],
 		module = [[[Module]
 Name=Moogle TTS
@@ -66,11 +66,11 @@ enabled=1]],
 		filepath = [[Moogle Scripts\Moogle FPS\Moogle FPS.lua]],
 		table = "return MoogleFPS",
 		url = [[https://github.com/KaliMinion/Moogle-Stuff/raw/master/MoogleFPS.lua]],
-		version = "1.2.5",
+		version = "1.2.6",
 		category = "Utility",
 		stability = "Working",
 		releasedate = os.time { year = 2017, month = 05, day = 29 },
-		lastupdate = 1516509692,
+		lastupdate = 1519063945,
 		info = [[An FPS overlay like Fraps or Nvidia's GeForce Experience. For people who have issue using Minion with other overlays.]],
 		module = [[[Module]
 Name=Moogle FPS
@@ -85,17 +85,36 @@ enabled=1]]
 		filepath = [[Moogle Scripts\Moogle PushButton\Moogle PushButton.lua]],
 		table = "return MooglePushButton",
 		url = [[https://github.com/KaliMinion/Moogle-Stuff/raw/master/MooglePushButton.lua]],
-		version = "1.0.0",
+		version = "1.0.1",
 		category = "Dev",
 		stability = "Working",
 		releasedate = os.time { year = 2018, month = 01, day = 25 },
-		lastupdate = 1516910177,
+		lastupdate = 1519064147,
 		info = [[A developer module like the one in Dev, but able to iterate PushButton presses.]],
 		module = [[[Module]
 Name=Moogle PushButton
 Dependencies=minionlib
 Version=1
 Files=Moogle PushButton.lua
+enabled=1]]
+	},
+	[7] = {
+		name = "Moogle Tweaks",
+		status = "open",
+		filepath = [[Moogle Scripts\Moogle Tweaks\Moogle Tweaks.lua]],
+		table = "return MoogleTweaks",
+		url = [[https://github.com/KaliMinion/Moogle-Stuff/raw/master/MoogleTweaks.lua]],
+		version = "1.0.0",
+		category = "Utility",
+		stability = "WiP but Working",
+		releasedate = os.time { year = 2018, month = 02, day = 19 },
+		lastupdate = 1519064468,
+		info = [[Navigate to NPCs and trade in items for stuffs, temp name, will make it better later.]],
+		module = [[[Module]
+Name=Moogle Tweaks
+Dependencies=minionlib
+Version=1
+Files=Moogle Tweaks.lua
 enabled=1]]
 	},
 }
