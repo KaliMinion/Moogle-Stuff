bdb7ac7fbbd4af58446fc2041810c50b3772feac
Kali <33697905+KaliMinion@users.noreply.github.com>
2018-03-17 03:00:23 -0500

    Update MoogleScripts2.lua

--- MoogleScripts2.lua
+++ MoogleScripts2.lua
@@ -29,55 +29,5 @@ local MoogleScripts = {
 		stability = [[Core Moogle Module]],
 		info = [[Where all my Moogle Functions are stored.]]
 	},
-	[4] = {
-		name = [[Moogle Text to Speech]],
-		status = [[open]],
-		filepath = [[Moogle Scripts\Moogle TTS.lua]],
-		table = [[return MoogleTTS]],
-		url = [[TestFolder/Moogle Scripts/Main Window]],
-		category = [[Utility]],
-		stability = [[WiP but Working]],
-		info = [[Narrates NPC dialog, as well as some other windows in game.]]
-	},
-	[5] = {
-		name = [[Moogle FPS]],
-		status = [[open]],
-		filepath = [[Moogle Scripts\Moogle FPS.lua]],
-		table = [[return MoogleFPS]],
-		url = [[TestFolder/Moogle Scripts/Moogle FPS]],
-		category = [[Utility]],
-		stability = [[Working]],
-		info = [[An FPS overlay like Fraps or Nvidia's GeForce Experience. For people who have issue using Minion with other overlays.]]
-	},
-	[6] = {
-		name = [[Moogle PushButton]],
-		status = [[open]],
-		filepath = [[Moogle Scripts\Moogle PushButton.lua]],
-		table = [[return MooglePushButton]],
-		url = [[TestFolder/Moogle Scripts/Moogle PushButton]],
-		category = [[Dev]],
-		stability = [[Working]],
-		info = [[A developer module like the one in Dev, but able to iterate PushButton presses.]]
-	},
-	[7] = {
-		name = [[Moogle Tweaks]],
-		status = [[open]],
-		filepath = [[Moogle Scripts\Moogle Tweaks.lua]],
-		table = [[return MoogleTweaks]],
-		url = [[TestFolder/Moogle Scripts/Moogle Tweaks]],
-		category = [[Utility]],
-		stability = [[WiP but Working]],
-		info = [[Navigate to NPCs and trade in items for stuffs, temp name, will make it better later.]]
-	},
-	[8] = {
-		name = [[Moogle MultiSession Manager]],
-		status = [[open]],
-		filepath = [[Moogle Scripts\Moogle MultiSession Manager.lua]],
-		table = [[return MoogleMultiSessionManager]],
-		url = [[TestFolder/Moogle Scripts/Moogle MultiSession Manager]],
-		category = [[Utility]],
-		stability = [[Beta]],
-		info = [[A module to monitor what other bot instances on your computer are doing, send commands to specific instances or all at the same time.]]
-	}
 }
 return MoogleScripts
